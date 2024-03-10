b1a177a27c7418a3e253d585e77ec7af2b171f4d
heyskanda <heyskanda@gmail.com>
2024-03-10 22:34:45 +0530

    Modified drawer file with DrawerContent

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,23 +1,81 @@
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import { DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
+import { useRouter } from 'expo-router';
 import * as React from 'react';
-import { Drawer } from 'react-native-paper';
+import { Text, View, StyleSheet, Image } from 'react-native';
+import { Drawer, useTheme } from 'react-native-paper';
 
-const MyComponent = () => {
-  const [active, setActive] = React.useState('');
+const DrawerContent = (props: any) => {
+  const router = useRouter()
+  const theme = useTheme()
 
   return (
-    <Drawer.Section title="Some title">
-      <Drawer.Item
-        label="First Item"
-        active={active === 'first'}
-        onPress={() => setActive('first')}
-      />
-      <Drawer.Item
-        label="Second Item"
-        active={active === 'second'}
-        onPress={() => setActive('second')}
-      />
-    </Drawer.Section>
+    <DrawerContentScrollView {...props}>
+      <View style={styles.container}>
+        <View style={styles.logo}>
+          <Ionicons name="logo-codepen" size={52} color="black" />
+        </View>
+
+        <Drawer.Section style={styles.drawerSection}>
+          <DrawerItem
+            icon={({ color, size }) => (
+              <MaterialCommunityIcons
+                name="account-outline"
+                color={color}
+                size={size}
+              />
+            )}
+            label="Profile"
+            onPress={() => router.push('/account')}
+          />
+          <DrawerItem
+            icon={({ color, size, focused }) => (
+              <MaterialCommunityIcons name="share-variant-outline" size={size} color={color} />
+            )}
+            label="Referral"
+            onPress={() => router.push('/referral')}
+          />
+          <DrawerItem
+            icon={({ color, size, focused }) => (
+              <MaterialCommunityIcons name="label-percent-outline" size={size} color={color} />
+            )}
+            label="Subscription"
+            onPress={() => router.push('/subscription')}
+          />
+          <DrawerItem
+            icon={({ color, size, focused }) => (
+              <MaterialCommunityIcons name="logout" size={size} color={color} />
+            )}
+            label="Logout"
+            onPress={() => router.push('/subscription')}
+          />
+        </Drawer.Section>
+      </View>
+
+      <View>
+        
+      </View>
+    </DrawerContentScrollView>
   );
 };
 
-export default MyComponent;
+export default DrawerContent;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  logo: {
+    padding: 12
+  },
+  profile: {
+    padding: 12,
+    borderRadius: 12,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  drawerSection: {
+    marginTop: 15,
+  },
+});
