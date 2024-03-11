d27fcbc2a2e130e3ab7bc9a3a80e4f6a5efaf794
heyskanda <heyskanda@gmail.com>
2024-03-11 17:22:12 +0530

    Back button to route back to the previous page

---
+++ components/Global/BackButton.tsx
@@ -0,0 +1,30 @@
+import { useAppTheme } from '@/app/_layout';
+import { MaterialIcons } from '@expo/vector-icons';
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+interface BackButtonProps {
+  onPress: () => void
+}
+
+const BackButton = ({
+  onPress,
+}: BackButtonProps) => {
+  const theme = useAppTheme()
+
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <MaterialIcons name="arrow-back-ios-new" size={21} color={theme.colors.secondary} />
+    </TouchableOpacity>
+  );
+};
+
+export default BackButton;
+
+const styles = StyleSheet.create({
+  button: {
+    marginLeft: 12,
+    padding: 12,
+  }
+});
