20ce650e16995a12789f4aba14361b8d7b963502
heyskanda <heyskanda@gmail.com>
2024-03-16 23:54:44 +0530

    Registration page initialized

---
+++ app/(auth)/register.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function register() {
+  return (
+    <View>
+      <Text>register</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+})
