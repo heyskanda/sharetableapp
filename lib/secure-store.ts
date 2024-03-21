7d42320d92f2ede2bb2cdae1c5a6b678eac9683a
heyskanda <heyskanda@gmail.com>
2024-03-21 17:11:59 +0530

    Theme utilities name changed to theme-provider

--- lib/secure-store.ts
+++ lib/secure-store.ts
@@ -2,17 +2,9 @@ import * as SecureStore from "expo-secure-store"
 
 export async function storeItem(key: string, value: any) {
     try {
-        return await SecureStore.setItemAsync(key, JSON.stringify(value))
+        return await SecureStore.setItemAsync(key, JSON.stringify(value, null, 2))
     } catch (error) {
         console.log("secure store set item error", error);
-    }
-}
-
-export async function getItem(key: string) {
-    try {
-        return await SecureStore.getItemAsync(key)
-    } catch (error) {
-        console.log("secure store get item error", error);
     }
 }
 
@@ -24,14 +16,10 @@ export async function removeItem(key: string) {
     }
 }
 
-export async function handleUserAuth() {
-    try {
-        const user = await getItem("authSession")
-        const userData = user ? JSON.parse(user) : null
-        console.log("session data", userData);
-        
-        return userData
-    } catch (error) {
-        console.log("session error", error)
-    }
+export async function userAuthSession() {
+    const user = await SecureStore.getItemAsync("authSession")
+    const userData = user ? JSON.parse(user) : null
+    console.log("session data", userData);
+    
+    return userData
 }
