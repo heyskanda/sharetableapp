383dad536016b0029c66ba654570dcf6bd1dcc97
heyskanda <heyskanda@gmail.com>
2024-03-27 23:48:43 +0530

    Added fetchLatestPriceOfIdea function. Updated query params of fetchAllIdeas.

--- lib/ideas.ts
+++ lib/ideas.ts
@@ -1,20 +1,22 @@
 import axios from "./axios";
 
-export async function fetchAllIdeas(callType: string) {
+export async function fetchAllIdeas(callType: string, skip: number, limit: number) {
     console.log('fetchAllIdeas');
     
     return await axios.get(
         `/recommendations/customer`,
         {
             params: {
-                callType: callType
+                callType: callType,
+                skip: skip,
+                limit: limit,
             }
         }
     )
     .then(res => res.data)
 }
 
-export async function fetchLatestIdeas(category: string) {
+export async function fetchLatestIdeas() {
     console.log('fetchLatestIdeas');
 
     return await axios.get(
@@ -28,6 +30,26 @@ export async function fetchIdeaById(id: string | string[]) {
     
     return await axios.get(
         `/recommendations/customer/${id}`
+    )
+    .then(res => res.data)
+}
+
+export async function fetchLatestPriceOfIdea(ideaData: any) {
+    console.log(ideaData, 'fetchLatestPriceOfIdea');
+    
+    return await axios.get(
+        `/shares/ltp/${ideaData?.instrument_key}`,
+        {
+            params: {
+                recommendation: {
+                    _id: ideaData?._id,
+                    segment: ideaData?.segment,
+                    instrument_key: ideaData?.instrument_key,
+                    trading_symbol: ideaData?.trading_symbol,
+                    last_price: ideaData?.last_price,
+                }
+            }
+        }
     )
     .then(res => res.data)
 }
